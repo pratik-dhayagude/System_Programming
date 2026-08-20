916fa5d066a6c5db87cf7873d110169e86b28782
Pratik Dhananjay Dhayagude <pratikdhaygude21@gmail.com>
2026-08-20 10:43:58 +0530

    Add files via upload

---
+++ Socket_Programming/Marvellous.h
@@ -0,0 +1,10 @@
+#include<stdio.h>		 // Strandred Input/Output
+#include<sys/socket.h>   // For creating the socket
+#include<netinet/in.h>   // For Internet Protocall
+#include<unistd.h>		 // Import the posix operation
+#include<arpa/inet.h>    // Internet operations and IP address manipulations.
+#include<stdlib.h>		 // It manages dynamic memory allocation, process control, conversions, and random numbers
+#include<string.h>		 // It provides functions to manipulate, copy, search, and measure null-terminated character arrays
+#include<libgen.h>      // POSIX-standard header used primarily for parsing pathname strings
+
+
